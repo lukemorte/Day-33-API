5d47860dbd25ee087ae8dcd9add0aa7b6eede67e
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-04 07:02:32 +0200

    implement quote fetching from Kanye API and update text formatting

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import tkinter
 import requests
 
 
+API_URL = "https://api.kanye.rest"
+
+
 def get_quote():
-    pass
+    response = requests.get(API_URL)
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
 
 
 # code
@@ -18,7 +25,7 @@ window.configure(padx=50, pady=50)
 canvas = tkinter.Canvas(width=300, height=414)
 background_img = tkinter.PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote goes HERE", fill="white", width=250, font=("Arial", 30, "bold"))
+quote_text = canvas.create_text(150, 207, text="Kanye Quote goes HERE", fill="white", width=250, font=("Arial", 24, "bold"))
 canvas.grid(column=0, row=0)
 
 kanye_img = tkinter.PhotoImage(file="kanye.png")
